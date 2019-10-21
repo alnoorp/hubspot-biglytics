6fb5a87e256211e3a5a0f08bc14c3a8d2910ea8e
Jason Rosa <jrosa@hubspot.com>
2019-10-21 15:50:07 -0400

    Adding styles to the blog post

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// Blog Comments Toggle
+
+var blogCommentsButton = document.querySelector(".blog-comments__toggle");
+
+if (blogCommentsButton) {
+    blogCommentsButton.addEventListener('click', function() {
+        var blogCommentsSection = document.querySelector(".blog-comments__listing"); 
+        blogCommentsSection.style.display = "block";
+    });
+}
