9fb7318efa7ca4e0d3fcccbdf86f493904328fc6
Jason Rosa <jrosa@hubspot.com>
2019-10-21 10:35:59 -0400

    Updated the blog listing and design

---
+++ src/modules/Blog Tag Filter.module/module.js
@@ -0,0 +1,8 @@
+var blogTagSelect = document.querySelector('.blog-tag-filter__dropdown select');
+
+blogTagSelect.addEventListener('change', function (event) {
+    if (blogTagSelect.value) {
+      window.location = blogTagSelect.value;
+    }
+    return false;
+  });
