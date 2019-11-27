c960d870039ff53283d3861638a08b2a4b4606b5
Jason Rosa <jrosa@hubspot.com>
2019-11-26 19:18:36 -0500

    Updating pricing table module

--- src/modules/Pricing Table.module/module.js
+++ src/modules/Pricing Table.module/module.js
@@ -1,23 +1,26 @@
 (function() {
-    var i; 
+
+    // Variables
+    var i;
     var pricingSelector = document.querySelectorAll(".billing-selectors__pricing-option input");
-    
+
+    // Event Listeners
     for (i=0; i < pricingSelector.length; i++) {
         pricingSelector[i].addEventListener("click", function(e){
-            e.preventDefault; 
+            e.preventDefault;
             var priceTarget = "price-block--" + this.getAttribute("id");
             var priceSection = this.parentElement.parentElement.nextElementSibling;
             var priceTables = priceSection.querySelectorAll(".price-block");
             for (i=0; i < priceTables.length; i++) {
                 if (priceTables[i].classList.contains(priceTarget)) {
-                    priceTables[i].style.display="block"; 
+                    priceTables[i].style.display="block";
                 }
                 else {
-                    priceTables[i].style.display="none"; 
+                    priceTables[i].style.display="none";
                 }
             }
-        }); 
+        });
     }
-  
+
 })();
 
