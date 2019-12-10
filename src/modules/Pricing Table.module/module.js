8dce3f6233d1ec0277449732a8981c99d72f8c67
Jason Rosa <jrosa@hubspot.com>
2019-12-10 10:21:58 -0500

    Running prettier on project

--- src/modules/Pricing Table.module/module.js
+++ src/modules/Pricing Table.module/module.js
@@ -1,26 +1,24 @@
 (function() {
+  // Variables
+  var i;
+  var pricingSelector = document.querySelectorAll(
+    '.billing-selectors__pricing-option input'
+  );
 
-    // Variables
-    var i;
-    var pricingSelector = document.querySelectorAll(".billing-selectors__pricing-option input");
-
-    // Event Listeners
-    for (i=0; i < pricingSelector.length; i++) {
-        pricingSelector[i].addEventListener("click", function(e){
-            e.preventDefault;
-            var priceTarget = "price-block--" + this.getAttribute("id");
-            var priceSection = this.parentElement.parentElement.nextElementSibling;
-            var priceTables = priceSection.querySelectorAll(".price-block");
-            for (i=0; i < priceTables.length; i++) {
-                if (priceTables[i].classList.contains(priceTarget)) {
-                    priceTables[i].style.display="block";
-                }
-                else {
-                    priceTables[i].style.display="none";
-                }
-            }
-        });
-    }
-
+  // Event Listeners
+  for (i = 0; i < pricingSelector.length; i++) {
+    pricingSelector[i].addEventListener('click', function(e) {
+      e.preventDefault;
+      var priceTarget = 'price-block--' + this.getAttribute('id');
+      var priceSection = this.parentElement.parentElement.nextElementSibling;
+      var priceTables = priceSection.querySelectorAll('.price-block');
+      for (i = 0; i < priceTables.length; i++) {
+        if (priceTables[i].classList.contains(priceTarget)) {
+          priceTables[i].style.display = 'block';
+        } else {
+          priceTables[i].style.display = 'none';
+        }
+      }
+    });
+  }
 })();
-
